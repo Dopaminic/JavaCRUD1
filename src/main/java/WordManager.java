0bd238044ce5adae9a855016369de8c9f6df1afe
Dopminic <matthew000218@gmail.com>
2023-09-12 16:15:48 +0900

    JavaCRUD project1

---
+++ src/main/java/WordManager.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class WordManager {
+    Scanner s = new Scanner(System.in);
+    WordCRUD wordCRUD;
+
+    WordManager() {
+        wordCRUD = new WordCRUD(s);
+    }
+
+    public int selectMenu(){
+        System.out.print("""
+                *** 영단어 마스터 ***
+                ********************
+                1. 모든 단어 보기
+                2. 수준별 단어 보기
+                3. 단어 검색
+                4. 단어 추가
+                5. 단어 수정
+                6. 단어 삭제
+                7. 파일 저장
+                0. 나가기
+                ********************
+                => 원하는 메뉴는?\s""");
+
+        return s.nextInt();
+    }
+
+    public void start() {
+        while (true) {
+            int menu = selectMenu();
+
+            if(menu == 0) break;
+            if(menu == 1) {            // create
+                wordCRUD.listAll();
+            }
+            else if(menu == 2){
+                wordCRUD.searchLevel();
+            }
+            else if(menu == 3){
+                wordCRUD.searchWord();
+            }
+            else if(menu == 4){        // list
+                wordCRUD.addWord();
+            }
+            else if(menu == 5){        // update
+                wordCRUD.updateItem();
+            }
+            else if(menu == 6){        // delete
+                wordCRUD.deleteItem();
+            }
+            else if(menu == 7){        // save data
+                wordCRUD.saveFile();
+            }
+
+        }
+    }
+}
